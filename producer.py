68f49bcaa8a98610849a31b34b74d94b6a63bdef
akshatism <akshatkumar.2013@gmail.com>
2018-09-21 23:47:33 -0400

    :tada: Adding Kafka Producer logic

--- producer.py
+++ producer.py
@@ -1,13 +1,14 @@
+import config
 from time import sleep
 from json import dumps
 from kafka import KafkaProducer
 
 # Setting broker details in the bootstrap_servers array and serializing data
-producer = KafkaProducer(bootstrap_servers=['localhost:9092'],
+producer = KafkaProducer(bootstrap_servers=[config.broker],
                          value_serializer=lambda x: dumps(x).encode('utf-8'))
 
 # Sending serializer data from producer to topic 'testing'
-for x in range(20):
-    data = {'number' : x}
-    producer.send('testing',data)
-    sleep(1)
+for y in range(config.iterations):
+    data = {'number': y}
+    producer.send(config.topic, data)
+    sleep(config.time)
