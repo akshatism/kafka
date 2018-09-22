3a9056a172dad5f3ab01d2382417161b9709b752
akshatism <akshatkumar.2013@gmail.com>
2018-09-21 23:48:01 -0400

    :tada: Adding Kafka Consumer logic

--- consumer.py
+++ consumer.py
@@ -1,21 +1,22 @@
+import config
 from pymongo import MongoClient
 from kafka import KafkaConsumer
 from json import loads
 
 # Setting client for MongoDB and creating a collection for writing the data
-client = MongoClient('localhost:27017')
+client = MongoClient(config.client)
 collection = client.testing.testing
 
-# Setting consumer details and deserializing data from utf-8 encoding to json
-consumer = KafkaConsumer('testing',
-                         bootstrap_servers=['localhost:9092'],
+# Setting consumer details and de-serializing data from utf-8 encoding to json
+consumer = KafkaConsumer(config.topic,
+                         bootstrap_servers=[config.broker],
                          auto_offset_reset='earliest',
                          enable_auto_commit=True,
-                         group_id="0",
+                         group_id='0',
                          value_deserializer=lambda x: loads(x.decode('utf-8')))
 
 # Consumer taking data from topic iteratively and writing it to MongoDB collection we specified above
 for msg in consumer:
     msg = msg.value
     collection.insert_one(msg)
-    print('{} added to {}'.format(msg,collection))
+    print('{} added to {}'.format(msg, collection))
